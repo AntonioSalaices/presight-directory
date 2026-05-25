370d8220cfb4eea1ecb649182e7c63c4f656e0a4
Antonio Salaices <antonio.salaices@yahoo.com>
2026-05-25 13:33:37 +0400

    [MOD] modify script for seed to populate db

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,5 +1,6 @@
-import Database from "better-sqlite3";
 import path from "path";
+
+import Database from "better-sqlite3";
 
 const db = new Database(path.join(__dirname, "../../presight.db"));
 
--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,4 +1,52 @@
+import { faker } from "@faker-js/faker";
+
 import db from "./database";
+
+const TOTAL_USERS = 1000;
+
+const HOBBIES = [
+  "padel",
+  "reading",
+  "gaming",
+  "cooking",
+  "hiking",
+  "painting",
+  "photography",
+  "music",
+  "traveling",
+  "gardening",
+  "cycling",
+  "yoga",
+  "swimming",
+  "writing",
+  "dancing",
+  "fishing",
+  "chess",
+  "knitting",
+  "running",
+  "surfing",
+  "coding",
+];
+
+const NATIONALITIES = [
+  "Mexican",
+  "Colombian",
+  "Russian",
+  "Mauritian",
+  "Spanish",
+  "Brazilian",
+  "British",
+  "Canadian",
+  "Aussie",
+  "French",
+  "German",
+  "Italian",
+  "Japanese",
+  "Indian",
+  "Chinese",
+  "Korean",
+  "Dutch",
+];
 
 const data = db.prepare("SELECT COUNT(*) as count FROM users").get() as {
   count: number;
@@ -6,9 +54,40 @@ const data = db.prepare("SELECT COUNT(*) as count FROM users").get() as {
 const hasData = data.count > 0;
 
 if (hasData) {
-  console.log("Ya tiene data");
+  console.log("Database is already seeded");
   process.exit(0);
 }
 
 //Insert data
-console.log("Khalas");
+const insertUser = db.prepare(
+  `INSERT INTO users (avatar, first_name, last_name, age, nationality)
+   VALUES (?, ?, ?, ?, ?) 
+`,
+);
+
+const insertHobby = db.prepare(
+  `INSERT INTO user_hobbies (user_id, hobby) VALUES (?, ?)`,
+);
+
+const implementSeed = db.transaction(() => {
+  for (let i = 0; i < TOTAL_USERS; i++) {
+    const result = insertUser.run(
+      faker.image.avatar(),
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.number.int({ min: 18, max: 80 }),
+      faker.helpers.arrayElement(NATIONALITIES),
+    );
+
+    const userId = result.lastInsertRowid;
+    const numHobbies = faker.number.int({ min: 0, max: 10 });
+    const userHobbies = faker.helpers.arrayElements(HOBBIES, numHobbies);
+
+    for (const hobby of userHobbies) {
+      insertHobby.run(userId, hobby);
+    }
+  }
+});
+
+implementSeed();
+console.log(`Seeded ${TOTAL_USERS} users successfully, khalas.`);
