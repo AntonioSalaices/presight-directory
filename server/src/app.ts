16d83235f38d202244c104c4b5059fbe1f5bf5af
Antonio Salaices <antonio.salaices@yahoo.com>
2026-05-26 17:05:31 +0400

    [ADD] add cors library and implements config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import "./db/database";
 import usersRouter from "./routes/users";
@@ -6,6 +7,12 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/users", usersRouter);
