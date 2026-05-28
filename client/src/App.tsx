0286d7f909ebf05e15f2405c075a87865a4ae277
Antonio Salaices <antonio.salaices@yahoo.com>
2026-05-28 15:31:42 +0400

    [MOD] add postcss config, and basic theme for styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Sidebar from "./components/Sidebar";
 import UserList from "./components/UserList";
+import Filters from "./components/Filters";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <div>Hola</div>
+      <div className="flex h-screen bg-surface overflow-hidden">
         <Sidebar />
-        <main>
-          <UserList></UserList>
-        </main>
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Filters />
+          <UserList />
+        </div>
       </div>
     </QueryClientProvider>
   );
